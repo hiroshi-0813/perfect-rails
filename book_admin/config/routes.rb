7379a85c7b3ea774549a2059c796cbf083fb2c9b
hiroshi-0813 <maru9948@gmail.com>
2021-04-03 20:12:48 +0900

    Rails-new

---
+++ book_admin/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destory"
+  resources :publishers
+  resource :profile, only: %i[edit update show]
+end
